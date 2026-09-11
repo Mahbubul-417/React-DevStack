74d11084b6dd1b5a7e95b8c345eafc13fedd5e22
Mahbubul-417 <251-15-417@diu.edu.bd>
2026-09-11 21:47:04 +0600

    Nav section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,27 +2,27 @@ import  logo  from "../assets/logo-text.png";
 
 const Nav = () => {
     return (
-        <div className="border-b border-gray-200">
+        <div className="border-b border-gray-200 ">
 
-        <nav className="flex justify-between gap-4 container mx-auto py-3  ">
+        <nav className="flex justify-between gap-4 container  mx-10 py-4 px-8  ">
 
-    <img src={logo} alt="" />
+    <img src={logo} className="h-9" alt="" />
     
     <ul className="flex gap-4 items-center text-slate-600 font-semibold">
-        <li className="text-[#DB2777]">Home</li>
-        <li>Technologies</li>
-        <li>Projects</li>
-        <li>About</li>
-        <li>Contact</li>
+    <li className="text-[#DB2777]"><a href="/home">Home</a></li>
+    <li><a href="/service.com">Technologies</a></li>
+    <li><a href="/projects">Projects</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
     </ul>
 
     <div className="flex gap-4">
-    <button className="hover:bg-gray-100">
+    <button className="px-4 py-2 rounded-4xl font-semibold hover:bg-gray-100">
         Sign In
     </button>
 
-    <button className="px-4 py-2 bg-pink-600 text-white rounded-4xl
-    hover:bg-pink-300">
+    <button className="px-4 py-2 bg-[#DB2777] text-white rounded-4xl
+    hover:bg-pink-300 ">
         Sign Up
     </button>
 </div>
